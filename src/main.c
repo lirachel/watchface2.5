9cfbf9d312f5abdca3bd0b41c7c20684804ade0c
lirachel <rachel.li.325@gmail.com>
2016-07-19 16:02:55 -0500

    changed percentage font

--- src/main.c
+++ src/main.c
@@ -117,11 +117,12 @@ static void main_window_load(Window *window) {
 // Set the compositing mode so that transparent images are transparent
   bitmap_layer_set_compositing_mode(bitmap_layer, GCompOpSet);
 	
-  s_battery_percentage = text_layer_create(GRect(23, 5, 144, 168));
+  s_battery_percentage = text_layer_create(GRect(23, 6, 144, 168));
   text_layer_set_background_color(s_battery_percentage, GColorClear);
   text_layer_set_text_color(s_battery_percentage, GColorWhite);
 	
-  text_layer_set_font(s_battery_percentage, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_font(s_battery_percentage, fonts_load_custom_font(
+                          resource_get_handle(RESOURCE_ID_GEOSANS_13)));
 	
 	
   //text_layer_set_text(s_battery_percentage, "");
